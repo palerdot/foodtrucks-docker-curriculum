fe27ea3c6c4ace72a60e64a14231a2160208b0e4
palerdot <palerdot@gmail.com>
2018-10-31 16:36:59 +0530

    [my changes]

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 # start from base
 FROM ubuntu:latest
-MAINTAINER Prakhar Srivastav <prakhar@prakhar.me>
+MAINTAINER Arun Kumar <palerdot@gmail.com>
 
 # install system-wide deps for python and node
 RUN apt-get -yqq update
--- setup-docker.sh
+++ setup-docker.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # build the flask container
-docker build -t prakhar1989/foodtrucks-web .
+docker build -t palerdot/foodtrucks-web .
 
 # create the network
 docker network create foodtrucks-net
 
 # start the ES container
-docker run -d --name es --net foodtrucks-net -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.3.2
+# give custom options so that elastic search does not give memory not enough error
+docker run -d --name es --net foodtrucks-net  -p 9200:9200 -p 9300:9300 -e ES_JAVA_OPTS="-Xms256m -Xmx256m" -e "http.host=0.0.0.0" -e "transport.host=127.0.0.1" -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.3.2
 
 # start the flask app container
 docker run -d --net foodtrucks-net -p 5000:5000 --name foodtrucks-web prakhar1989/foodtrucks-web
